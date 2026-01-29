a9aa9442d71761ef60b938e7856e91543d6d1689
probonopd <probonopd@users.noreply.github.com>
2026-01-29 22:34:30 +0100

    Add private method declarations in Eau.h

--- Eau.h
+++ Eau.h
@@ -38,8 +38,17 @@
                      in: (NSCell*)cell
 			            state: (GSThemeControlState) state;
 
+/* Private methods */
+- (NSGradient *) _bezelGradientWithColor:(NSColor*) baseColor;
+- (NSBezierPath*) buttonBezierPathWithRect: (NSRect)frame andStyle: (int) style;
+- (NSGradient *) _windowTitlebarGradient;
+- (NSRect) drawInnerGrayBezel: (NSRect)border withClip: (NSRect)clip;
+- (NSGradient *) _buttonGradientWithColor:(NSColor*) baseColor;
+
 /* Safely convert colors to calibrated RGB. Use this where code previously used
    colorUsingColorSpaceName: NSCalibratedRGBColorSpace to avoid exceptions when
    colors are in non-RGB color spaces (pattern, device, etc.). */
 NSColor *EauSafeCalibratedRGB(NSColor *c);
+
+@end
 
